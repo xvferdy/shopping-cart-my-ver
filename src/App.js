35b7700fa4d1759dc153535a05c8e0f8724ad2df
xvferdy <xv.ferdy@gmail.com>
2021-06-29 13:01:26 +0800

    h1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import CartList from "./components/CartList";
+import ProductList from "./components/ProductList";
+
+//context provider
+import { CartProvider } from "./context/Carts.context";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Belajar Context dan useReducer
-        </a>
-      </header>
+      <CartProvider>
+        <CartList />
+        <hr />
+        <ProductList />
+      </CartProvider>
     </div>
   );
 }
